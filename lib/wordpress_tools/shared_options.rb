55c5d836f7560152595957c164cb18a3bf815623
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-02-20 18:56:18 +0100

    Add :boolean type to bare option

--- lib/wordpress_tools/shared_options.rb
+++ lib/wordpress_tools/shared_options.rb
@@ -5,15 +5,15 @@ module WordPressTools
     class_methods do
       def shared_options
         [
-          [:force, {type: :boolean, desc: "Overwrite existing WP-CLI / WP-CLI Server installation"}],
-          [:locale, { aliases: "-l", desc: "WordPress locale", default: Configuration.for(:locale)}],
-          [:bare, {aliases: "-b", desc: "Remove default themes and plugins"}],
-          [:admin_user, { desc: "WordPress admin user", default: Configuration.for(:admin_user)}],
-          [:admin_email, {desc: "WordPress admin email", default: Configuration.for(:admin_email)}],
-          [:admin_password, {desc: "WordPress admin password", default: Configuration.for(:admin_password)}],
-          [:db_user, {desc: "MySQL database user", default: Configuration.for(:db_user)}],
-          [:db_password, {desc: "MySQL database pasword", default: Configuration.for(:db_password)}],
-          [:site_url, {desc: "Wordpress site URL", default: Configuration.for(:site_url)}]
+          [:force, { type: :boolean, desc: "Overwrite existing WP-CLI / WP-CLI Server installation" }],
+          [:locale, { aliases: "-l", desc: "WordPress locale", default: Configuration.for(:locale) }],
+          [:bare, { type: :boolean, aliases: "-b", desc: "Remove default themes and plugins" }],
+          [:admin_user, { desc: "WordPress admin user", default: Configuration.for(:admin_user) }],
+          [:admin_email, { desc: "WordPress admin email", default: Configuration.for(:admin_email) }],
+          [:admin_password, { desc: "WordPress admin password", default: Configuration.for(:admin_password) }],
+          [:db_user, { desc: "MySQL database user", default: Configuration.for(:db_user) }],
+          [:db_password, { desc: "MySQL database pasword", default: Configuration.for(:db_password) }],
+          [:site_url, { desc: "Wordpress site URL", default: Configuration.for(:site_url) }]
         ]
       end
 
