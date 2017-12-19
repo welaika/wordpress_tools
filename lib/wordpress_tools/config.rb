3a51602440b97f38534dc72624da38e204d4807b
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-12-19 08:41:06 +0100

    remove unused config and templates (wp server command)

--- lib/wordpress_tools/config.rb
+++ lib/wordpress_tools/config.rb
@@ -1,10 +1,8 @@
 module WordPressTools
   class Configuration
-
     DEFAULT_CONFIG = {
       wp_cli_download_url: "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar",
       wp_cli_path: "/usr/local/bin/wp",
-      wp_cli_config_path: "~/.wp-cli/config.yml",
       admin_user: "admin",
       admin_email: "admin@example.com",
       admin_password: "password",
@@ -17,6 +15,5 @@ module WordPressTools
     def self.for(key)
       DEFAULT_CONFIG[key]
     end
-
   end
 end
